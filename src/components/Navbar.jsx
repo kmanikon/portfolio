8174750c9451b817d2c8351cc9f13525d2bb7ce9
Bob Shens <kiranmanikonda123@gmail.com>
2023-07-06 22:08:53 -0700

    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -155,7 +155,7 @@ const Navbar = () => {
               className='flex justify-between items-center w-full text-gray-300'
               href='mailto: kiranmanikonda123@gmail.com'
             >
-              Email <HiOutlineMail size={30} />
+              Email <HiOutlineMail size={30} /> 
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
