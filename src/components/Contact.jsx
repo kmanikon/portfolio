624fd2cc6e741076bdea1e16da260902c9d9d949
Bob Shens <kiranmanikonda123@gmail.com>
2024-06-07 23:40:29 -0700

    Centered Contact Text on Mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/b38fe819-4ff7-4e08-a1a8-b81ba50cad1b" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className=' text-4xl font-bold inline text-gray-300 flex justify-center'>Contact</p>
-                <p className='text-gray-300 py-4 flex justify-center'>Feel free to shoot me an email - kiranmanikonda123@gmail.com</p>
+                <p className=' text-gray-300 py-4 flex justify-center' style={{wordBreak: 'break-word', textAlign: 'center'}}>Feel free to shoot me an email - kiranmanikonda123@gmail.com</p>
             </div>
             <input className='bg-[#edebeb] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#edebeb]' type="email" placeholder='Email' name='email' />
