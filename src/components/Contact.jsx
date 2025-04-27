3a823e80fb90b9cac0ba242b3cbecf5cb8beb0e5
kmanikon <103885541+kmanikon@users.noreply.github.com>
2025-04-27 05:59:17 -0500

    Updated context field colors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,15 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/b38fe819-4ff7-4e08-a1a8-b81ba50cad1b" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className=' text-4xl font-bold inline text-gray-300 flex justify-center'>Contact</p>
-                <p className=' text-gray-300 py-4 flex justify-center' style={{wordBreak: 'break-word', textAlign: 'center'}}>Feel free to shoot me an email - kiranmanikonda123@gmail.com</p>
+                <p className=' text-gray-300 py-4 justify-center' style={{wordBreak: 'break-word', textAlign: 'center'}}>
+                  <p>
+                    Feel free to shoot me an email - kiranmanikonda123@gmail.com
+                  </p>
+                </p>
             </div>
-            <input className='bg-[#edebeb] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#edebeb]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#edebeb] p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <input className='bg-[#d3d3d3] p-2 placeholder-gray-800' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#d3d3d3] placeholder-gray-800' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#d3d3d3] p-2 placeholder-gray-800' name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Submit</button>
         </form>
     </div>
